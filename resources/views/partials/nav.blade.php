c3b0a1202b46002ab914343af962a9e9378e7336
Fred-S <fredvantage@gmail.com>
2020-09-15 15:31:35 +0200

    added tailwind

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,7 @@
+<header class="flex items-center justify-around flex-wrap border-black px-4 py-4">
+    <div>
+      
+          <div class="logo"><a href="/"><img src="/img/Logo-512.png" class="fill-current h-8 w-8 mr-2 h-18 w-18"></a></div>
+      
+    </div> <!-- end nav -->
+</header>
