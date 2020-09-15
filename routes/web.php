3ad22f0f111b0d53977510fd85f81018962fb4d8
Fred-S <fredvantage@gmail.com>
2020-09-15 17:21:12 +0200

    added bootstrap4

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
